0e884d4dffa0f0eaa2486a1aecf4147623a68876
Dani <danielvediaj@gmail.com>
2023-03-05 11:46:57 +0100

    arg-parsing

---
+++ Argparsing/argp.py
@@ -0,0 +1,23 @@
+# Line Interfaces With Python's argparse
+
+# https://realpython.com/command-line-interfaces-python-argparse/#getting-to-know-command-line-interfaces
+
+
+import sys
+from pathlib import Path
+
+if (args_count := len(sys.argv)) > 2:
+    print(f"One argument expected, got {args_count - 1}")
+    raise SystemExit(2)
+elif args_count < 2:
+    print("You must specify the target directory")
+    raise SystemExit(2)
+
+target_dir = Path(sys.argv[1])
+
+if not target_dir.is_dir():
+    print("The target directory doesn't exist")
+    raise SystemExit(1)
+
+for entry in target_dir.iterdir():
+    print(entry.name)
